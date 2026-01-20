c2a29e5443d15e58af89ca10d0d569ad7d091713
René Rivas <reinaldorrivas@gmail.com>
2026-01-20 13:50:44 -0400

    fh-modern-js: Added some changes in store

--- src/users/store/users.store.js
+++ src/users/store/users.store.js
@@ -1,20 +1,27 @@
+import { User } from "../models/user.model";
 import { loadUsersByPage } from "../use-cases/loadUsersByPage";
 
 const state = {
   currentPage: 0,
+  firstPage: 0,
+  lastPage: 0,
   users: [],
 };
 
 const nextPage = async () => {
+  if (state.lastPage && state.currentPage === state.lastPage) return;
   const users = await loadUsersByPage(state.currentPage + 1);
-  if (!users.length) return;
 
   state.currentPage += 1;
   state.users = users;
 };
 
 const previousPage = async () => {
-  throw new Error("Not implemented.");
+  if (state.currentPage === 1) return;
+  const users = await loadUsersByPage(state.currentPage - 1);
+
+  state.currentPage -= 1;
+  state.users = users;
 };
 
 const onUserChanged = async () => {
@@ -25,11 +32,39 @@ const reloadPage = async () => {
   throw new Error("Not implemented.");
 };
 
+const updateFirstPage = (firstPage) => {
+  state.firstPage = firstPage;
+};
+
+const updateLastPage = (lastPage) => {
+  state.lastPage = lastPage;
+};
+
 export default {
   nextPage,
   onUserChanged,
   previousPage,
   reloadPage,
+  /**
+   *
+   * @returns {number}
+   */
+  getFirstPage: () => state.firstPage,
+  /**
+   *
+   * @returns {number}
+   */
+  getLastPage: () => state.lastPage,
+  updateFirstPage,
+  updateLastPage,
+  /**
+   *
+   * @returns {User[]}
+   */
   getUsers: () => structuredClone(state.users),
+  /**
+   *
+   * @returns {number}
+   */
   getCurrentPage: () => state.currentPage,
 };
